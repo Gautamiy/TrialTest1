03e1260b2fffa74c0d076f6bf85692f0e4f3b703
DS_USER <DS_USER@DESKTOP-RHB79IL>
2022-10-14 19:29:02 +0530

    addition program

---
+++ GitHubProject/src/addn.java
@@ -0,0 +1,16 @@
+
+
+public class addn {
+	public void addition() {
+		int a=6;
+		int b=3;
+	   int c= a+b;
+	   System.out.println("addition of two no is: "+c );
+        		
+	}
+	public static void main(String[]args) {
+		addn ad=new addn();
+	   ad.addition();
+	}
+
+}
